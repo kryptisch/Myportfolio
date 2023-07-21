68aa812687468005d541ab326705e0f654bbf32b
kryptisch <kryptischfunk@gmail.com>
2023-07-21 11:35:05 +0530

    removed unused components

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,4 +1,4 @@
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+export { BallCanvas, ComputersCanvas,};
